404c70f2568e1b484c273ce4a3f8cbf6832262a6
Eliana Rosselli <elianarosselli@gmail.com>
2019-03-13 13:55:02 -0300

    cambie el arreglo de viajes en usuario.hpp por un vector

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -1,7 +1,7 @@
 
 #include "DtFecha.hpp"
 #include "Viaje.hpp"
-
+#include <vector>
 #include <string>
 
 class Usuario {
@@ -9,7 +9,7 @@ private:
     std::string ci, nombre;
     DtFecha fecha_ingreso;
     int cant_viajes; //tope del arreglo viajes[100]
-    Viaje viajes[100];
+    std::vector<Viaje> viajes[100];
 public:
     std::string getCi();
     std::string getNombre();

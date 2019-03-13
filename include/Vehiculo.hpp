3b05f122e1284556072afc68a5dc4db44d76b203
Eliana Rosselli <elianarosselli@gmail.com>
2019-03-13 14:23:02 -0300

    implementacion vehiculo.cpp
    faltan los setters de las subclases

--- include/Vehiculo.hpp
+++ include/Vehiculo.hpp
@@ -6,9 +6,14 @@ private:
     int nro_serie;
     float porcentaje_bateria, precio_base;
 public:
+	//setters
 	void set_nro_serie(int);
 	void set_porcentaje_bateria(float);
 	void set_precio_base(float);
+	//getters
+	int get_nro_serie();
+	float get_porcentaje_bateria();
+	float get_precio_base();
     virtual float darPrecioViaje(int, int) = 0;
 };
 
@@ -18,6 +23,7 @@ private:
     bool tieneLuces;
 public:
 	Monopatin(int, float, float, bool=false);
+	bool get_luces();
     float darPrecioViaje(int, int);
     ~Monopatin();
 };
@@ -29,6 +35,8 @@ private:
     int cant_cambios;
 public:
 	Bicicleta(int, float, float, TipoBici, int);
+	TipoBici get_tipo();
+	int get_cant_cambios();
     float darPrecioViaje(int, int);
     ~Bicicleta();
 };
--- src/Vehiculo.cpp
+++ src/Vehiculo.cpp
@@ -1,2 +1,82 @@
 
 #include "../include/Vehiculo.hpp"
+#include "../include/TipoBici.hpp"
+
+//clase vehiculo (abstracta)
+
+//setters
+void Vehiculo::set_nro_serie(int num){
+	this->nro_serie=num;
+}
+
+void Vehiculo::set_porcentaje_bateria(float bateria){
+	this->porcentaje_bateria=bateria;
+}
+
+void Vehiculo::set_precio_base(float pb){
+	this->precio_base=pb;
+}
+
+//getters
+int Vehiculo::get_nro_serie(){
+	return this->nro_serie;
+}
+
+float Vehiculo::get_porcentaje_bateria(){
+	return this->porcentaje_bateria;
+}
+
+float Vehiculo::get_precio_base(){
+	return this->precio_base;
+}
+
+
+//clase Monopatin
+
+//constructor con parametros
+Monopatin::Monopatin(int num, float bateria, float pb, bool luces){
+	set_nro_serie(num);
+	set_porcentaje_bateria(bateria);
+	set_precio_base(pb);
+	this->tieneLuces=luces;
+
+}
+
+//getter
+bool Monopatin::get_luces(){
+	return this->tieneLuces;
+}
+
+
+float Monopatin::darPrecioViaje(int duracion, int distancia){
+	if (tieneLuces)
+		return ((0.5*duracion) + (distancia*get_precio_base()));
+	else //no tiene luces 
+		return distancia*get_precio_base();
+}
+
+//clase Bicicleta
+
+//constructor con parametros
+Bicicleta::Bicicleta(int num, int bateria, int pb, TipoBici bici, int cambios){
+	set_nro_serie(num);
+	set_porcentaje_bateria(bateria);
+	set_precio_base(pb);
+	this->tipo=bici;
+	this->cant_cambios=cambios;
+}
+
+//getters
+TipoBici Bicicleta::get_tipo(){
+	return this->tipo;
+}
+
+int Bicicleta::get_cant_cambios(){
+	return this->cant_cambios;
+}
+
+float Bicicleta::darPrecioViaje(int duracion, int distancia){
+	return (get_precio_base()*distancia)
+}
+
+

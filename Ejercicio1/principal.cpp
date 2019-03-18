2ac22009a0c09d392725c2e49580ec31d161da41
Seba Lagomarsino <lagomarsinosebastian@gmail.com>
2019-03-17 21:09:04 -0300

    Agregado descripciones a main.cpp y cambiado nombre a principal.cpp

--- Ejercicio1/principal.cpp
+++ Ejercicio1/principal.cpp
@@ -19,24 +19,77 @@ Usuario arreglo_usuarios[MAX_USUARIOS];
 //arreglo de punteros a vehiculos?
 Vehiculo* arreglo_vehiculos[MAX_VEHICULOS];
 
+/*
+Registra un en el sistema.
+La fecha de ingreso se obtiene del reloj de la máquina.
+Si existe un usuario registrado con la misma cédula, se levanta una excepción std::invalid_argument.
+*/
 void registrarUsuario(std::string ci, std::string nombre) {
 
 }
+
+/*
+Agrega un nuevo vehículo al sistema.
+Controlar que se cumplen:
+(1) no existe un vehículo con el mismo número de serie,
+(2) porcentaje como valor entre 0 y 100 y
+(3) precio base positivo.
+De no ser así, se levanta una excepción std::invalid_argument.
+*/
 void agregarVehiculo(const DtVehiculo& vehiculo) {
 
 }
+
+/*
+Crea un viaje entre el usuario y el vehículo identificados por ci y nroSerieVehiculo, respectivamente.
+Controlar que se cumplen:
+(1) existe un usuario registrado con esa ci,
+(2) existe un vehículo registrado con ese nro serie,
+(3) duración y distancia positivas y
+(4) fecha del viaje posterior o igual a la fecha de ingreso del usuario.
+De no ser así, se levanta una excepción std::invalid_argument.
+*/
 void ingresarViaje(string ci, int nroSerieVehiculo, const DtViajeBase& viaje) {
 
 }
+
+
+/*
+Devuelve un arreglo con información detallada de los viajes
+realizados por el usuario antes de cierta fecha.
+Para poder implementar esta operación se deberá sobrecargar
+el operador < (menor que) del tipo de datos (data type) DtFecha.
+cantViajes es un parámetro de salida donde se devuelve la cantidad
+de viajes encontrados (corresponde a la cantidad de valores DtViaje que se devuelven).
+*/
 DtViaje** verViajesAntesDeFecha(const DtFecha& fecha, string ci, int& cantViajes) {
     return nullptr;
 }
+
+/*
+Elimina los viajes del usuario identificado por ci,
+realizados en la fecha ingresada.
+Si no existe un usuario registrado con esa cédula,
+se levanta una excepción std::invalid_argument.
+*/
 void eliminarViajes(string ci, const DtFecha& fecha) {
 
 }
+
+/*
+Modifica el porcentaje de carga de la bateria del vehículo identificado por nroSerieVehiculo.
+En caso de que el vehículo no exista,
+o la carga ingresada no se encuentre entre 0 y 100 se levanta una excepción std::invalid_argument.
+*/
 void cambiarBateriaVehiculo(int nroSerieVehiculo, float cargaVehiculo) {
 
 }
+
+/*
+Devuelve un arreglo con los vehículos del sistema.
+cantVehiculos es un parámetro de salida donde se
+devuelve la cantidad de vehículos (corresponde a la cantidad de valores DtVehiculo que se devuelven).
+*/
 DtVehiculo** obtenerVehiculos(int& cantVehiculos) {
     return nullptr;
 }
@@ -77,15 +130,15 @@ int main() {
             break;
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
     return 0;
 }

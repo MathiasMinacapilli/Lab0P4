4a4f1694d59723be2058227c987999d813a7ecde
Mathias <mathiasminacapilli@gmail.com>
2019-03-17 22:34:15 -0300

    Sobrecargar del operador >= en DtFecha

--- Ejercicio1/src/DtFecha.cpp
+++ Ejercicio1/src/DtFecha.cpp
@@ -25,3 +25,13 @@ int DtFecha::getAnio() {
     /* Retorna el anio del datavalue DtFecha */
     return this->anio;
 }
+
+//Sobrecarga el operador >=
+bool operator>=(DtFecha fecha1, DtFecha fecha2) {
+    if(fecha1.getAnio() != fecha2.getAnio())
+        return fecha1.getAnio() >= fecha2.getAnio();
+    else if(fecha1.getMes() != fecha2.getMes())
+        return fecha1.getMes() >= fecha2.getMes();
+    else 
+        return fecha1.getDia() >= fecha2.getDia();
+}
